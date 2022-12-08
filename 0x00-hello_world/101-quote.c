49a9873135f8f6ed7298900509946dc60f2e8be2
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-08 06:24:30 -0800

    using write insteadof printf and puts functions

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print message without using puts or print functions
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	 write(2, "and that piece of art is useful\" \- Dora Korpar, 2015-10-19");
+	return (1);
+}
