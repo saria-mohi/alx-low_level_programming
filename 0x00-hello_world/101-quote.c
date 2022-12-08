b3193cc2cf8909e12a916a6df62ae7abb9add8d4
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-08 07:27:34 -0800

    using write insteadof printf and puts functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	/*sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));*/
 	return (1);
 }
