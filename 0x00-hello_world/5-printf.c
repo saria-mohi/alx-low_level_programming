95bf2006be65d71c592b2b0fe1e5760c145c324a
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-08 04:11:42 -0800

    Write a C program that use printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - print line of code using printf
+ *
+ * Description: the code print message by using printf fucntion
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
