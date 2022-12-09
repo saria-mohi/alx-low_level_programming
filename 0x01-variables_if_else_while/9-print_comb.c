90c218ee027eb605698c44e200de68e2665b703a
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-09 10:40:14 -0800

    print all combination number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - pprint combination number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
