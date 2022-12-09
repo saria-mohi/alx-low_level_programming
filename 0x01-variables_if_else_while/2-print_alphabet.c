30166a9643689139cf55cd9239a2663bf8c77327
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-09 08:05:36 -0800

    print lower case character from a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+/**
+ * main - print lowrcase character
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		printf("\n");
+	}
+	return (0);
+}
