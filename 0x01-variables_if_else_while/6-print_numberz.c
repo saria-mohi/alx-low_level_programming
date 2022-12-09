0e184f7b4c6d286ad657c8cb6b664665e5d10b6f
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-09 10:05:13 -0800

    use putchar to print number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar((num % 10) + '0');
+		putchar(num);
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
