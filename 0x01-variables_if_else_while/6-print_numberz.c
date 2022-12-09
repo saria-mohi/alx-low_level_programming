035c81f249946edac3b0706acab1c8298c7b859a
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-09 09:58:58 -0800

    print number from 0 to 9 using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num < 10; num++)
 	{
-		putchar(num);
+		putchar((num % 10) + '0');
 	}
 	printf("\n");
 	return (0);
