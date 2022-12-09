73d6f9af52a46bdfc8f569bbb384a2500bc3a7f5
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-09 09:12:27 -0800

    print all numbert from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print all numbers between 0 and 10
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
