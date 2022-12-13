419288958980a03a1399287f5ddca8337f1a9063
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-13 08:47:47 -0800

    print time tabl from 0 to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,24 +4,30 @@
 */
 void times_table(void)
 {
-	int i, j;
+	int i, j, t;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			int t = i * j;
-
-			if ((t / 10) > 0)
-				_putchar((t / 10) + '0');
-			else
-				_putchar(' ');
-			_putchar((t % 10) + '0');
-			if (j < 9)
+			t = (i * j);
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
+			if (t >= 10)
+			{
+				_putchar((t / 10) + '0');
+				_putchar((t % 10) + '0');
+			}
+			else if (t < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((t % 10) + '0');
+			}
+			else
+				_putchar((t % 10) + '0');
 		}
 		_putchar('\n');
 	}
