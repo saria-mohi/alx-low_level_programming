90f1fbebd40e0237d2f6c7ec854980406625f08f
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-13 02:28:11 -0800

    using _putchar to print _putchar characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,13 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char ch[] = "_putchar";
+	int i;
+
+	for (i = 0; i < sizeof(ch); i++)
+	{
+		_putchar(ch[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
