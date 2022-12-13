d23bfc66765db0309c24d6a70e73e2741a1393d6
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-13 08:59:16 -0800

    summ 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - sum and return 2 numbers
+ * @num1: is number 1 wanna to add it
+ * @num2: is number 2 wanna to add it
+ * Return: result of Sum
+*/
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print sum of 2 number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int result;
+
+	result = add(89, 9);
+	printf("%d\n", result);
+	return (0);
+}
