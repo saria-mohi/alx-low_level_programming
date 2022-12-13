1d9e1aa3b5cc129e37016fa7148f9864a99f442c
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-13 12:19:18 -0800

    finds and prints the first 98 Fibonacci numbers,

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,41 +6,18 @@
 */
 int main(void)
 {
-	int count;
-	unsigned long i, j, k;
-	unsigned long m, n, p, carry;
+	unsigned long int t0 = 0, t1 = 1, s;
+	int i;
 
-	count = 0;
-	i = 0;
-	j = 0;
-	for (count = 1; count <= 91; count++)
+	for (i = 1; i <= 98; i++)
 	{
-		k = i + j;
-		i = j;
-		j = k;
-		printf("%lu, ", k);
-	}
-	m = i % 1000;
-	i = i / 1000;
-	n = j % 1000;
-	j = j / 1000;
-	while (count <= 98)
-	{
-		carry = (m + n) / 1000;
-		p = (m + n) - carry * 1000;
-		k = (i + j) + carry;
-		m = n;
-		n = p;
-		i = j;
-		j = k;
-		if (p >= 100)
-			printf("%lu%lu", k, p);
+		s = t0 + t1;
+		if (i != 98)
+			printf("%lu, ", s);
 		else
-			printf("%lu0%lu", k, p);
-		if (count != 98)
-			printf(", ");
-		count++;
+			printf("%lu\n", s);
+		t0 = t1;
+		t1 = s;
 	}
-	putchar('\n');
-	return (0);
+	return (0)
 }
