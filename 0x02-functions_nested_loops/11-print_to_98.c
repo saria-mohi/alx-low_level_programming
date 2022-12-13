c80b0f8b19d28ce5a34d41baf93e50bef14a6ff6
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-13 11:00:29 -0800

    print all numbers between n and 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print all number between n to 98
+ *@n: is number start to count
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
