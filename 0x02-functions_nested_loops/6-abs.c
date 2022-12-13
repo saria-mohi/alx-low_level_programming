e617cb7e6f865f8ae60cd7c021b52ade5373d7f5
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-13 04:49:57 -0800

    print absoute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - print absloute value
+ * @n: is number wan to absloute
+ * Return: abslout value
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <math.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -6,3 +7,4 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
