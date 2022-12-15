86ee1738ab839b1c75acf1ee4d4c2ac5de64de02
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-15 05:41:09 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - print square using # character
+ * @size: is the number as size of square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
