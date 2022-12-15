ef30026d217021711b146a97d6df7da7ec4901e2
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-15 02:15:13 -0800

    print all number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - print all number from 0 to 9 using _putchar
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
