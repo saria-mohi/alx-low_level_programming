5d9950dc391f5e6b5ec9f38eed0748333420df4f
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-15 06:01:28 -0800

    fuzzzzzzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main  - print fizz bizz word when number multiple 3 and 5 and both
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			c = '\n';
+		else
+			c = ' ';
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz%c", c);
+		else if (i % 3 == 0)
+			printf("Fizz%c", c);
+		else if (i % 5 == 0)
+			printf("Buzz%c",c);
+		else
+			printf("%i%c", i, c);
+	}
+	return (0);
+}
