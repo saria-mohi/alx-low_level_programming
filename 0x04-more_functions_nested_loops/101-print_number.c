8f92be13f7a1ccc9712da63e16517b3d80f5be48
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-15 08:22:50 -0800

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,16 @@
  */
 void print_number(int n)
 {
+	int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
 	}
 	if (n == 0)
 		_putchar('0');
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (num / 10)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
 }
