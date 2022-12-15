130332f0aeb5c629c3111a06acca2b1b6ff5548a
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-15 04:56:45 -0800

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * print_line - print the _ as n times
- * @n: is number to drwa line from \
+ * print_diagonal - print \ as diagonal line
+ * @n: is number as time to print \
  */
 
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
