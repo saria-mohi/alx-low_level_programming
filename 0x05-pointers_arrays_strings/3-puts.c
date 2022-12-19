15c4dce8106d74bd1473d48398880929ee0949da
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-19 00:44:09 -0800

    print String

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - function to print the String
+ * @str: is a String wanna to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
