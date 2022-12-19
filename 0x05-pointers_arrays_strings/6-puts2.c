34b7a91ca55617e010b60ff3e2fe5733db891a08
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-19 04:38:57 -0800

    print other char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,9 +5,15 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+		_putchar(stri[i]);
 	_putchar('\n');
 }
