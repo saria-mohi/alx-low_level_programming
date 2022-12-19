bd63843b809f60e4a4ba7a0a082372c102594ab6
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-19 00:35:27 -0800

    print size of String

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include " main.h"
+/**
+ * _strlen - function to check size of '/string
+ * @s: is string
+ * Return: size of String
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
