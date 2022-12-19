46ae3b75cc608574390079b445b7a259749b9160
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-19 09:31:26 -0800

    password randomly

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,26 +8,35 @@
  */
 int main(void)
 {
-	int i, n, s;
+	int i, pass1, pass2, c;
 
-	s = 0;
 	char password[84];
 
 	srand(time(NULL));
 
-	for (i = 0; i < 100; i++)
+	while (c < 2772)
 	{
-		password[i] = rand() % 78;
-		s += (password[i] + '0');
-		putchar(password[i] + '0');
-		putchar(password[i] + '0');
-		if ((2772 - s) - '0' < 78)
+		password[i] = 33 + rand() % 94;
+		c += password[i++];
+	}
+	password[i] = '\0';
+
+	if (c != 2772)
+	{
+		pass1 = (c - 2772) / 2;
+		pass2 = (c - 2772) / 2;
+		if ((c - 2772) % 2 != 0)
+			pass1++;
+		for (i = 0; password[i]; i++)
 		{
-			n = 2772 - s - '0';
-			s += n;
-			putchar(n + '0');
-			break;
+			if (password[i] >= (33 + pass2))
+			{
+				password[i] -= pass2;
+				break;
+			}
 		}
 	}
+	printf("%s", password);
+
 	return (0);
 }
