135e01f7ad5cf158e4aed551bbf9635f4a0e3368
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-21 11:06:31 -0800

    convert lowr to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,16 +6,15 @@
  */
 char *string_toupper(char *str)
 {
-	int i, ch;
-
-	ch = 'a' - 'A';
+	char *result = str;
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] > 'a' && str[i] < 'z')
 		{
-			str[i] = str[i] - ch;
+			str[i] = str[i] - 32;
 		}
 	}
-	return (str);
+	return (result);
 }
