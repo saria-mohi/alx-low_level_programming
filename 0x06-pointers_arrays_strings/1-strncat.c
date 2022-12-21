1f32282c656c8cccda85247d4e3b5593dc73e746
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-21 09:16:36 -0800

    Write a function that concatenates two strings with n byte

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: String 1
+ * @src: String 2
+ * @n: length
+ * Return: Return a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, count;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (count = 0; count < n && src[count] != '\0'; count++)
+		dest[i] = src[count];
+	if (count < n)
+		dest[i] = '\0';
+	return (dest);
+}
