37a374111f57d41f0ea4bd0d703ec178042f01b1
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-21 12:16:14 -0800

    encode string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,12 +6,12 @@
  */
 char *leet(char *str)
 {
-	char ch[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L', '\0'};
+	char ch[] = "aAeEoOtTlL"
 	char num[] = "4433007711";
 
 	int i, j;
 
-	for (i = 0; str[i] != '\0'; ++i)
+	for (i = 0; str[i] != '\0'; i++)
 		for (j = 0; j < 9; j++)
 			if (str[i] == ch[j])
 				str[i] = num[j];
