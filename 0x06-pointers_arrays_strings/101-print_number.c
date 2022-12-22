fd92e808449e8f05390adcab3bc6ecb6e8356939
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-22 00:38:44 -0800

    int number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - print the number
+ * @n: is a number
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if (num / 10)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
+}
