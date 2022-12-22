3d129bc5ee3760e7b7cb6790c8370e078e937591
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2022-12-22 12:51:29 +0200

    Update 103-infinite_add.c
    add 2 numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,4 +9,77 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+  int count = 0, i = 0, j;
+	char *s1 = n1, *s2 = n2;
+
+	while (*s1 != 0)
+		s1++;
+	while (*s2 != 0)
+		s2++;
+	size_r--;
+	r[size_r] = 0;
+	s1--;
+	s2--;
+	while (s2 != n2 - 1 && s1 != n1 - 1)
+	{
+		r[i] = *s2 - '0' + *s1 + count;
+		count = 0;
+		if (r[i] > '9')
+		{
+			count++;
+			r[i] -= 10;
+		}
+		i++;
+		s2--;
+		s1--;
+		if (size_r == i && (s1 != n1 - 1 || s2 != n2 - 1 || count == 1))
+			return (0);
+	}
+	while (s1 != n1 - 1)
+	{
+		r[i] = *s1 + count;
+		count = 0;
+		if (r[i] > '9')
+		{
+			count = 1;
+			r[i] -= 10;
+		}
+		s1--;
+		i++;
+		if (size_r == i && (s1 != n1 - 1 ||  count == 1))
+			return (0);
+	}
+	while (s2 != n2 - 1)
+	{
+		r[i] = *s2 + count;
+		count = 0;
+		if (r[i] > '9')
+		{
+			count = 1;
+			r[i] -= 10;
+		}
+		s2--;
+		index++;
+		if (size_r == i && (s2 != n2 - 1 || count == 1))
+			return (0);
+	}
+	if (count == 1)
+	{
+		r[i] = '1';
+		r[i + 1] = 0;
+	}
+	else
+	{
+		r[i--] = 0;
+	}
+	index2 = 0;
+	while (j <= i)
+	{
+		count = r[i];
+		r[i] = r[j];
+		r[j] = count;
+		i--;
+		j++;
+	}
+	return (r);
 }
