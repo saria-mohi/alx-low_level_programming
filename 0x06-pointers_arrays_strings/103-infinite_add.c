5ed600692f9c4135618aa586b2fce660b4d85968
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2022-12-22 13:00:35 +0200

    Update 103-infinite_add.c
    add numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,77 +9,28 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int count = 0, i = 0, j;
-	char *s1 = n1, *s2 = n2;
-
-	while (*s1 != 0)
-		s1++;
-	while (*s2 != 0)
-		s2++;
-	size_r--;
-	r[size_r] = 0;
-	s1--;
-	s2--;
-	while (s2 != n2 - 1 && s1 != n1 - 1)
+	int i, j, n;
+	
+	i = j = n = 0;
+	
+	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
 	{
-		r[i] = *s2 - '0' + *s1 + count;
-		count = 0;
-		if (r[i] > '9')
-		{
-			count++;
-			r[i] -= 10;
-		}
+		r[0] = 1 + '0';
+		j = 1;
+	}
+	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
+	{
+		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
+			n = 1;
+		else
+			n = 0;
+		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
+		r[j] = r[j] % 10 + '0';
 		i++;
-		s2--;
-		s1--;
-		if (size_r == i && (s1 != n1 - 1 || s2 != n2 - 1 || count == 1))
-			return (0);
-	}
-	while (s1 != n1 - 1)
-	{
-		r[i] = *s1 + count;
-		count = 0;
-		if (r[i] > '9')
-		{
-			count = 1;
-			r[i] -= 10;
-		}
-		s1--;
-		i++;
-		if (size_r == i && (s1 != n1 - 1 ||  count == 1))
-			return (0);
-	}
-	while (s2 != n2 - 1)
-	{
-		r[i] = *s2 + count;
-		count = 0;
-		if (r[i] > '9')
-		{
-			count = 1;
-			r[i] -= 10;
-		}
-		s2--;
-		i++;
-		if (size_r == i && (s2 != n2 - 1 || count == 1))
-			return (0);
-	}
-	if (count == 1)
-	{
-		r[i] = '1';
-		r[i + 1] = 0;
-	}
-	else
-	{
-		r[i--] = 0;
-	}
-	j = 0;
-	while (j <= i)
-	{
-		count = r[i];
-		r[i] = r[j];
-		r[j] = count;
-		i--;
 		j++;
+		if (n1[i] == '\0' || n2[i] == '\0')
+			r[j] = '\0';
 	}
+	r[j] = '\0';
 	return (r);
 }
