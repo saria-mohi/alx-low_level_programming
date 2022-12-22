87d3c2c9c783682fcdd4e623e82c3647c9b4ad38
saria-mohi <saria.m.mohammed@gmail.com>
2022-12-22 02:46:18 -0800

    add numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * infinite_add - function to add 2 number
+ * @n1: number1
+ * @n2: number2
+ * @r: to save result
+ * @size_r: buffer size
+ * Return: r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
