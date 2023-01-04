8e545f6adde092596f7a87c30696251c9379d12d
saria-mohi <saria.m.mohammed@gmail.com>
2023-01-04 12:01:57 -0800

    find sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * squareroot - function to check the natural square root
+ * @n: natural square root
+ * @c: counter
+ * Return: if n is natural sqrt or not
+ */
+int squareroot(int n, int c)
+{
+	if (c < 1)
+		return (-1);
+	else if (n == c * c)
+		return (c);
+	else
+		return (squareroot(n, c - 1));
+}
+/**
+ * _sqrt_recursion - function to print sqrt
+ * @n: is number wanna to calculate sqrt
+ * Return: the sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
+}
