05070e6be6671a277c154415060045c8a42eadb7
saria-mohi <saria.m.mohammed@gmail.com>
2023-01-04 13:27:42 -0800

    is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,8 +3,8 @@
  * palindrome - checks if start and end of string matches
  * @str: string
  * @st: start of string 0
- * @end: end of string from is_palindrome, from lengthc
- * Return: if str is a palindrome
+ * @end: end of string from is_palindrome, from length
+ * Return: number present 1 or 0
  */
 int palindrome(char str[], int st, int end)
 {
@@ -16,6 +16,7 @@ int palindrome(char str[], int st, int end)
 		return (palindrome(str, st + 1, end - 1));
 	return (1);
 }
+
 /**
  * lengthc - finds the length count
  * @s: input
@@ -27,6 +28,7 @@ int lengthc(char *s)
 		return (1 + lengthc(s + 1));
 	return (0);
 }
+
 /**
  * is_palindrome - Entry Point
  * @s: input
