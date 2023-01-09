d62d49e84f948ffa9fdbf7a15b94b1db1e443f79
saria-mohi <saria.m.mohammed@gmail.com>
2023-01-09 06:31:52 -0800

    split String in to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+/**strtow - function split string in to sub string
+ * @str: is String wanna to split it
+ * Return: NULL if str == NULL or str == "" or word f String
+ */
+char **strtow(char *str)
+{
+
