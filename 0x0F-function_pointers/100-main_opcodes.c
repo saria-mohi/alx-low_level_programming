3073612268ac04a451ea6164155e5f06de5e6c0a
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2023-01-18 19:58:03 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+**
+ * print_opcodes - print the opcode
+ * @a: address of the main function
+ * @n: number of bytes to print
+ */
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+}
+
 /**
- * print_opcodes - print the opcodes of this program
+ * main - function to prints the opcodes
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: O
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
+}
