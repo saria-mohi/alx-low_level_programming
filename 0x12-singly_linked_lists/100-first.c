24b3b60dc6256fdfbeba4b4f41a728c4b46fa24c
saria-mohi <saria.m.mohammed@gmail.com>
2023-01-26 05:57:49 -0800

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,38 @@
+#include <stdio.h>
+/**
+ * first - prints a sentence before the main
+ *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * bmain - function executed before main
  * Return: no return.
  */
-
 void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
