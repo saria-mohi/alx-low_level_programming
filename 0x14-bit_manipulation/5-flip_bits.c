7ba696df729bca61924b41036cca36a4e3af1b19
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2023-02-02 10:00:38 +0200

    flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * flip_bits - flip to get from one number to another.
+ * flip_bits - function to flip to get from one number to another.
  * @n: the first number
  * @m: the second number
  * Return: the number of bits you would need to flip
