df90dfc722fb3ebc894fb1b4d25293554192c9ec
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2023-02-02 09:59:20 +0200

    set the value of a bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit - function to set the value of a bit
  * @index: starting from 0 of the bit you want to set
  * @n: pointer number of i
- * Return: 1 if it worked, or -1 if an error occurred
+ * Return: 1 if it worked, or -1 if not
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
